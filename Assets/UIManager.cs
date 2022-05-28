360bbbe2faa48acfdd89f59ecf028bc1189b6318
Matthew O'Connell <oconnellomj@gmail.com>
2022-05-27 21:10:15 -0400

    backup git

---
+++ Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    public Text name, age, location, gender, FavoriteCompany;
+
+    private Name _name;
+    private AGL _agl;
+    private Company _company;
+
+    private void Start()
+    {
+        _name = GetComponent<Name>();
+        _agl = GameObject.FindObjectOfType<AGL>();
+        _company = GameObject.FindObjectOfType<Company>();
+
+        name.text = "Name: " + _name.name;
+        age.text = "Age: " + _agl.age;
+        location.text = "Location: " + _agl.location;
+        gender.text = "Gender: " + _agl.gender;
+        FavoriteCompany.text = "Favorite Company: " + _company.favoriteCompany;
+
+    }
+}
